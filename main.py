585585fe3ab881fdeec998d21feac818134733be
Dong Jae Koh <48575816+ehdwo98@users.noreply.github.com>
2021-08-11 07:26:45 +0900

    Update main.py
    Macro

--- main.py
+++ main.py
@@ -15,9 +15,9 @@ def closePopup():
 
 # 로그인
 def login():
-    driver.find_element_by_name('user.stu_nbr').send_keys('2018112801')
-    driver.find_element_by_name('user.usr_id').send_keys('ehdwo98')
-    driver.find_element_by_name('user.passwd').send_keys('wlsdk2991!')
+    driver.find_element_by_name('user.stu_nbr').send_keys('학번 입력')
+    driver.find_element_by_name('user.usr_id').send_keys('아이디 입력')
+    driver.find_element_by_name('user.passwd').send_keys('비번 입력')
     driver.find_element_by_xpath('//*[@id="loginForm"]/table/tbody/tr[4]/td/button[1]').click()
     driver.implicitly_wait(5)
 
@@ -42,21 +42,21 @@ login_time = time.time()
 
 while(True):
     # 수강 신청 인원 비는지 확인
-    max_people = int((driver.find_element_by_xpath('/html/body/div/div[3]/div[11]/div/div[2]/table/tbody/tr[3]/td[8]')).text)
-    current_people = int((driver.find_element_by_xpath('//*[@id="lectPackReqGrid_1"]/td[9]')).text)
+    max_people = int((driver.find_element_by_xpath('꽉찬 수꾸 강좌의 제한인원 xpath가져오기')).text)
+    current_people = int((driver.find_element_by_xpath('꽉찬 수꾸 강좌의 현재인원 xpath가져오기')).text)
     
     # 현재 신청 인원이 비면
     if(max_people > current_people):
         # 강의 취소
-        lecture = driver.find_element_by_xpath('//*[@id="onlineLectReqGrid_5"]/td[2]')
+        lecture = driver.find_element_by_xpath('수강목록에서 수취할 과목 교과목번호 xpath가져오기')
         if(lecture.text == 'CLTR003002'):
-            driver.find_element_by_xpath('//*[@id="onlineLectReqGrid_5"]/td[1]/input').click()
-            driver.find_element_by_xpath('//*[@id="btn_delete"]').click()
+            driver.find_element_by_xpath('수강목록에서 수취할 과목 삭제선택 xpath가져오기').click()
+            driver.find_element_by_xpath('수강신청 삭제버튼 xpaht가져오기').click()
             closeAlert()
             closeAlert()
         
             # 강의 신청
-            driver.find_element_by_xpath('//*[@id="lectPackReqGrid_1"]/td[11]').click()
+            driver.find_element_by_xpath('신청할 수꾸강좌 신청 xpath가져오기').click()
             closeAlert()
             print("강의 신청 확인")
             # 강의 신청되면 반복문 정지
